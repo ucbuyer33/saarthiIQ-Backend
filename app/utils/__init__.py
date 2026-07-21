02186b9b9e963bbf7688e7e042c6c11860830967
ucbuyer33 <ucbuyer33@gmail.com>
2026-07-21 21:59:14 +0530

    fix: remove stale ROLE_PREFIX re-export from utils/__init__.py (deleted in single-role refactor)

--- app/utils/__init__.py
+++ app/utils/__init__.py
@@ -1,3 +1,3 @@
-from app.utils.id_gen import generate_user_id, ROLE_PREFIX
+from app.utils.id_gen import generate_user_id
 
-__all__ = ["generate_user_id", "ROLE_PREFIX"]
+__all__ = ["generate_user_id"]
